17a48e082bd5a49a003027a612d389a15fbe209a
Rukayat08 <mustapharukayat08@gmail.com>
2025-06-18 22:53:38 +0100

    Update eggproduction.py

--- eggproduction.py
+++ eggproduction.py
@@ -54,8 +54,8 @@ st.plotly_chart(organic, use_container_width = True)
 organic = px.bar(df["Number of eggs from hens in organic, free range farms"], y = "Number of eggs from hens in organic, free range farms", title = "Organic farms production")
 st.plotly_chart(organic, use_container_width = True)
 
-n-organic = px.bar(df["Number of eggs from hens in non-organic, free range farms"], y = "Number of eggs from hens in non-organic, free range farms", title = "Non-organic Farms production")
-st.plotly_chart(n-organic, use_container_width = True)
+n_organic = px.bar(df["Number of eggs from hens in non-organic, free range farms"], y = "Number of eggs from hens in non-organic, free range farms", title = "Non-organic Farms production")
+st.plotly_chart(n_organic, use_container_width = True)
 
 st.markdown("Bivariate Analysis")
 st.markdown("## Year vs Organic Farm prodcution")
